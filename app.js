40c2b9a2ef3cad458424e88f397dd44d287b5a8a
rholibobo <rholipop@gmail.com>
2022-12-04 02:21:03 +0100

    testing a live page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = 4002;
+const port = process.env.PORT || 4002;
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
 const bodyParser = require("body-parser");
